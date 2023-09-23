6c21cb6bfca0bd1318f805cc5440ed8230b05eaf
ferdawus <ferdawusa@gmail.com>
2023-09-23 08:11:46 +0600

    first commit

---
+++ vue/vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      greet: "Hi",
+      name: "ferdawus",
+      channel: "<b>CodeEvulations</b>",
+    };
+  },
+};
+</script>
+
+<template>
+  <div v-text="greet"></div>
+  <div v-html="channel"></div>
+  <p v-text="name"></p>
+</template>
+
+<style></style>
